e8a7bfa95a9f4b6902267e7d2d6b5d05359da457
Laurent Milne <lmilne360@gmail.com>
2017-03-19 20:24:20 -0400

    Setup PubNub API keys

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+angular.module('mbzapp', []);
+
 angular
-  .module('mbzapp', []);
+    .module('mbzapp')
+    .constant('config', {
+        rltm: {
+            service: 'pubnub',
+            config: {
+                publishKey: 'pub-c-2d34f44c-4371-4e45-8dda-1baf1ef3ec6e',
+                subscribeKey: 'sub-c-c57de3e6-0d01-11e7-83b6-0619f8945a4f'
+            }
+        }
+    });
